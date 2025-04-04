a3934c30b7f58668e3029d3b6c666bd74c1efcb7
Kunal <kunalkashiweb@gmail.com>
2025-04-04 17:55:17 +0530

    modified:   package.json

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "https://task-manager-ah04.onrender.com";
+  const serverUrl = "http://localhost:8000";
 
   const router = useRouter();
 
