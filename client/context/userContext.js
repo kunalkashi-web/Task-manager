ec9aca9368c1045d1797e7aca4d59319f334642a
Kunal <kunalkashiweb@gmail.com>
2025-04-04 18:04:47 +0530

    feat: server deployed

--- client/context/userContext.js
+++ client/context/userContext.js
@@ -9,7 +9,7 @@ const UserContext = React.createContext();
 axios.defaults.withCredentials = true;
 
 export const UserContextProvider = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://task-manager-virid-zeta.vercel.app";
 
   const router = useRouter();
 
